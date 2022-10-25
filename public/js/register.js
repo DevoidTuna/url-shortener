1f39a9e2b2436791f95055b28f282f8d00610109
DevoidTuna <patrick.brining@outlook.com>
2022-10-24 22:07:18 -0300

    save

--- public/js/register.js
+++ public/js/register.js
@@ -1,18 +1,67 @@
-let nameInput = document.getElementById('register-name').value
-let emailInput = document.getElementById('register-email').value
-let passwordInput = document.getElementById('register-password').value
-let confirmPasswordInput = document.getElementById('confirm-password').value
-let registerButton = document.getElementById('btn-register')
-
-
-
 function check() {
-    while(
-        nameInput.length > 3 &&
-        emailInput.length > 0 &&
-        passwordInput.length > 5 &&
-        confirmPasswordInput.length > 5
-    ) {
-        registerButton.style.backgroundColor = 'blue'
+    var input = {
+        name: document.getElementById('register-name'),
+        email: document.getElementById('register-email'),
+        password: document.getElementById('register-password'),
+        confirmPassword: document.getElementById('confirm-password'),
+        register: document.getElementById('btn-register')
     }
+
+    var span = {
+        name: document.getElementById('span-name'),
+        email: document.getElementById('span-email'),
+        password: document.getElementById('span-password'),
+        confirmPassword: document.getElementById('span-confirmPassword')
+    }
+
+    var form = document.querySelector('#form-register')
+
+    //     if(input.email.value.length > 1) {
+    //         span.email.style.visibility = 'visible'
+    //     } else {
+    //         span.email.style.visibility = 'hidden'
+    //     }
+
+    //     if(input.password.value.length < 6) {
+    //         span.password.style.visibility = 'visible'
+    //     } else {
+    //         span.password.style.visibility = 'hidden'
+    //     }
+
+    //     if(input.confirmPassword.value !== input.password.value) {
+    //         span.confirmPassword.style.visibility = 'visible'
+    //     } else {
+    //         span.confirmPassword.style.visibility = 'hidden'
+    //     }
+
+    //     if(
+    //         input.name.value.length > 2 &&
+    //         // validateEmail(input.email.value) &&
+    //         input.confirmPassword.value === input.password.value
+    //     ) {
+    //         form.submit()
+    //     }
+    // })
+
+    // function validateEmail(input) {
+
+    // var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  
+    // // if (input.value.match(validRegex)) {
+    // //   return true;
+    // // } else {
+    // //   return false;
+    // // }
+    // input.value.match(validRegex) ?  true : false
+    // }
 }
+
+input.register.addEventListener('click', function() {
+
+    console.log(
+        input.name,          
+        input.email,
+        input.password,
+        input.confirmPassword
+    )
+})
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,7 +1,8 @@
+@section('content')
+<!DOCTYPE html>
 <head>
     <link rel="stylesheet" href="{{ URL::asset('css/register.css'); }}">
 </head>
-@section('content')
 @auth
 {{route('page.user')}}
 @endauth
@@ -9,34 +10,37 @@
 @guest
 <div class="container-register">
     <h2>Register.</h2>
-    <form action="{{ route('do-register') }}" method="POST">
+    <form action="{{ route('do-register') }}" method="POST" id="form-register">
         @csrf
         <div id="boxes-container">
             <div class="box">
                 <label for="register-name">Name:</label>
-                <input type="text" name="name" id="register-name" oninput="check()" required>
+                <input type="text" name="name" id="register-name" oninput="check()" placeholder="Jake Nathan" required>
+                <span id="span-name">Your name need to have 3 or more letters</span>
             </div>
             <div class="box">
                 <label for="register-email">E-mail: </label>
-                <input type="email" name="email" id="register-email" oninput="check()" required>
+                <input type="email" name="email" id="register-email" oninput="check()" placeholder="Your email address" required>
+                <span id="span-email">This email address isn't valid</span>
             </div>
             <div class="box">
                 <label for="register-password">Password:</label>
-                <input type="password" name="password" id="register-password" oninput="check()" required>
+                <input type="password" name="password" id="register-password" oninput="check()" placeholder=" Create password (min 6 letters)" required>
+                <span id="span-password">The password must be 6 or more characters</span>
             </div>
             <div class="box">
                 <label for="confirm-password">Confirm<br>Password:</label>
-                <input type="password" name="confirm-password" id="confirm-password" oninput="check()" required>
+                <input type="password" name="confirm-password" id="confirm-password" oninput="check()" placeholder="Enter your password again" required>
+                <span id="span-confirmPassword">The passwords entered do not match</span>
             </div>
             <div class="bottom-buttons">
                 <a href="{{ route('page.login') }}">Have an account?</a><br>
-                <button type="submit" name="btn-register" id="btn-register">REGISTER</button>
+                <button type="button" name="btn-register" id="btn-register" disabled>REGISTER</button>
             </div>
         </div>
     </form>
 </div>
 @endguest
-
-@endsection
 <script src="{{ URL::asset('js/register.js'); }}"></script>
+@endsection
 @include('layout')
